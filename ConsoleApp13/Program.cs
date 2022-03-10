48f6b91c22d9b244c864ba2d79e7bb43d048beb8
KenanNagiyev <788lnmp@code.edu.az>
2022-03-10 11:29:57 +0400

    new file

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, -2, 7, -8 };
+            Console.WriteLine(MusMen(nums));
+        }
+        static int MusMen(int[] nums)
+        {
+            int menfi = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < 0)
+                {
+                    menfi = nums[i] * (-1);
+                    Console.WriteLine(menfi);
+
+                }
+                else
+                {
+                    Console.WriteLine(nums[i]);
+                }
+
+            }
+            return 0;
+        }
+    }
+}
